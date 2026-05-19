48dd2c7f18f8f250aa836955400b8c47e6d28635
Zinmin Aung <zinminaung@Zinmins-MacBook-Air.local>
2026-05-19 08:32:08 +0700

    finish pointofsale component static

--- client/src/pages/PointOfSale.tsx
+++ client/src/pages/PointOfSale.tsx
@@ -133,16 +133,44 @@ const PointOfSale = () => {
                 </div>
             </div>
 
-            <div className="component h-64 sm:h-full w-full sm:w-fit shrink-0 flex flex-col">
+            <div className="component sm:h-full w-full sm:w-fit shrink-0 flex flex-col">
                 <div className="flex justify-between gap-10 mini-component p-4">
                     <span className="text-nowrap">Current Order</span> 
                     <span className="size-5 aspect-square bg-teal-500 flex justify-center items-center rounded-full">0</span>
                 </div>
-                <div className='flex-1 flex flex-col p-2 justify-center items-center '>
+                <div className='flex-1 flex flex-col p-2 overflow-scroll justify-center items-center mini-component text-gray-400'>
                     <ShoppingBag size={30}/>
                     <span>Cart is Empty</span>
                     <span>Tap products to add</span>
                 </div>
+
+                <div className='p-4 pb-10 text-sm text-gray-400'>
+                    <div className='flex flex-col gap-1'>
+                        <div className='flex justify-between'>
+                            <span>Subtotal</span>
+                            <span>$0.0</span>
+                        </div>
+                        <div className='flex justify-between'>
+                            <span>Tax (7%)</span>
+                            <span>$0.0</span>
+                        </div>
+                        <div className='flex justify-between mini-component pb-4'>
+                            <span>Discount</span>
+                            <span>$0.0</span>
+                        </div>
+
+                        <div className='flex justify-between'>
+                            <span>Total</span>
+                            <span>$0.0</span>
+                        </div>
+                        <div className='flex justify-between gap-2'>
+                            <button className='border mini-component rounded-md w-full text-center'>Clear</button>
+                            <span className='border mini-component rounded-md w-full text-center'>Discount</span>
+                        </div>
+                        <button className='p-2 bg-teal-600 text-black rounded-md w-full'>Check Out</button>
+                    </div>                        
+                </div>
+`
             </div>
 
         </section>
